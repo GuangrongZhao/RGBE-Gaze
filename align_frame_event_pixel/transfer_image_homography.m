446ce2959b4f2f661bba926170ad47c731978159
GuangrongZhao <45409518+GuangrongZhao@users.noreply.github.com>
2024-03-17 22:03:11 +0800

    Add files via upload

--- align_frame_event_pixel/transfer_image_homography.m
+++ align_frame_event_pixel/transfer_image_homography.m
@@ -1,42 +1,34 @@
 clc
 close all
 clear all
-
-mse_list = [];
-load  D:\remote_dataset\calibration\CheckerboardPoints\tform_total.mat
-for user_num =79:79
-    user_num
-    for exp =2:2
-        exp
-        folder_path1 = ['C:\Users\Aiot_Server\Desktop\remote_dataset\user_',num2str(user_num),'\exp',num2str(exp),'\flir\'];
-        files1 = dir(fullfile(folder_path1, '*.png'));
-        names_file1 = sort_nat({files1.name}); %每一个aps图片的名字
-        
-        output_path =  ['C:\Users\Aiot_Server\Desktop\remote_dataset\user_',num2str(user_num),'\exp',num2str(exp),'\convert2eventspace\'];
+projectpath = input('Enter project location(such as /home/remote_apperance_gaze_dataset): ', 's');
+load ([projectpath, '/align_frame_event_pixel/tform_total.mat'])
+for user_num =1:1
+    for exp = 1:1
+        folder_path1 = [projectpath, '/raw_data/user_',num2str(user_num),'/exp',num2str(exp),'/flir/'];
+        files1 = dir(fullfile(folder_path1, '*.jpg'));
+        names_file1 = sort_nat({files1.name});
+        output_path =  [projectpath, '/raw_data/user_',num2str(user_num),'/exp',num2str(exp),'/convert2eventspace/'];
         if ~exist(output_path, 'dir')
-            % 如果目录不存在，则创建目录
             mkdir(output_path);
-            disp('目录创建成功！');
+            disp('The directory was created successfully!');
         end
-
         
-        parfor i = 1:length(files1)
-%             i
+        for i = 1:length(files1)
             file_path1 = cell2mat(fullfile(folder_path1, names_file1(i)));
             I1 = imread(file_path1);
-%             I1 = rgb2gray(I1);
+            
             I1= fliplr(I1);
             I1 = imresize(I1, [ 720 1280], 'Method', 'bicubic');
-            % figure(2)
-            % imshow(I1) % 翻转加变形后的rgb 图片
+            %figure(2)
+            %imshow(I1)
             
             outputView = imref2d(size(I1));
-            J = imwarp(I1, tform, 'OutputView', outputView); % 映射后的rgb 图像
+            J = imwarp(I1, tform, 'OutputView', outputView);
             
-            
-            %             figure(3);
-            %             imshow(J);
-            %             title('Transformed Image');
+            %figure(3);
+            %imshow(J);
+            %title('Transformed Image');
             output_path_each = cell2mat(fullfile(output_path, names_file1(i)));
             imwrite(J,output_path_each );
             
